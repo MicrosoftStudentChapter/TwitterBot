a3aed79b02b81733cfde2a8be88923dc6ee35fe5
Aryan-401 <aryan.raj.garg@gmail.com>
2022-06-23 21:07:04 +0530

    Added Code which would make integrating with the Discord Bot easier

--- reddit/reddit-comp.py
+++ reddit/reddit-comp.py
@@ -16,11 +16,12 @@ reddit = praw.Reddit(
     user_agent=getenv("USER_AGENT"),
 )
 
-connection = sqlite3.connect('news.db')
+connection = sqlite3.connect('twitterBot.db')
 cursor = connection.cursor()
 cursor.execute(
     '''CREATE TABLE IF NOT EXISTS News(ID VARCHAR(10) PRIMARY KEY, SUBREDDIT VARCHAR(50), FLAIR VARCHAR(50) DEFAULT 'None', 
-    TITLE VARCHAR(500) UNIQUE, URL VARCHAR(255) UNIQUE, RATIO FLOAT, SCORE INT, TOTALCOMMENTS INT, IMPORTANCE INT);''')  # table schema
+    TITLE VARCHAR(500) UNIQUE, URL VARCHAR(255) UNIQUE, RATIO FLOAT, SCORE INT, TOTALCOMMENTS INT, IMPORTANCE INT, SEEN BITINT);''')  # table schema
+connection.commit()
 
 subreddit = reddit.subreddit(subreddit_str)
 for submission in subreddit.top(limit=25, time_filter="day"):  # top 25 posts of the day across mentioned subreddits
@@ -28,11 +29,12 @@ for submission in subreddit.top(limit=25, time_filter="day"):  # top 25 posts of
             'business' not in submission.link_flair_text.lower()):  # no business tech news
         interest_factor = 1 + len(set(submission.title.lower().split()).intersection(interesting_keywords)) - (
             len(set(submission.title.lower().split()).intersection(boring_keywords)))
+
         info = (
             submission.id, submission.subreddit.display_name, submission.link_flair_text, submission.title,
             submission.url, submission.upvote_ratio, submission.score, submission.num_comments,
-            int(interest_factor * (submission.upvote_ratio * submission.score + submission.num_comments)))
+            int(interest_factor * (submission.upvote_ratio * submission.score + submission.num_comments)), 0)
 
-        cursor.execute(f'''INSERT OR REPLACE INTO News VALUES (?,?,?,?,?,?,?,?,?)''', info)  # insert into database
+        cursor.execute(f'''INSERT OR REPLACE INTO News VALUES (?,?,?,?,?,?,?,?,?,?)''', info)  # insert into database
 connection.commit()
 cursor.close()
