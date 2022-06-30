74d4d58c36fee191f8d93f078802f279d578f7cc
Aryan-401 <aryan.raj.garg@gmail.com>
2022-07-01 02:13:50 +0530

    Commented Unnecessary code, Added purge_user command, Added poll Command

--- discord_bot/cogs/moderation.py
+++ discord_bot/cogs/moderation.py
@@ -1,32 +1,33 @@
 # import datetime
+import datetime
 import re
 import discord
 from discord.ext import commands
 
 from discord_bot import error_messages
 
-TIME_REGEX = re.compile(r"(?:(\d{1,5})([hsmdw]))+?")
-TIME_DICT = {"h": 3600, "s": 1, "m": 60, "d": 86400, "w": 604800}
-
+# TIME_REGEX = re.compile(r"(?:(\d{1,5})([hsmdw]))+?")
+# TIME_DICT = {"h": 3600, "s": 1, "m": 60, "d": 86400, "w": 604800}
+NUM_DICT = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
 
 def success():
     return discord.Embed(title="Your wish is my command", description="That was easy 😎")
 
 
-def convert(argument):
-    args = argument.lower()
-    matches = re.findall(TIME_REGEX, args)
-    time = 0
-    for key, value in matches:
-        try:
-            time += TIME_DICT[value] * float(key)
-        except KeyError:
-            raise commands.BadArgument(
-                f"{value} is an invalid time key! h|m|s|d|w are valid arguments"
-            )
-        except ValueError:
-            raise commands.BadArgument(f"{key} is not a number!")
-    return round(time)
+# def convert(argument):
+#     args = argument.lower()
+#     matches = re.findall(TIME_REGEX, args)
+#     time = 0
+#     for key, value in matches:
+#         try:
+#             time += TIME_DICT[value] * float(key)
+#         except KeyError:
+#             raise commands.BadArgument(
+#                 f"{value} is an invalid time key! h|m|s|d|w are valid arguments"
+#             )
+#         except ValueError:
+#             raise commands.BadArgument(f"{key} is not a number!")
+#     return round(time)
 
 
 class Moderation(commands.Cog):
@@ -63,22 +64,30 @@ class Moderation(commands.Cog):
         await success_message.add_reaction('✔')
 
     # @commands.command(help='Give someone a Timeout (max 28 days)')
-    # @commands.check_any(commands.has_permissions(kick_members=True), commands.is_owner())
-    # async def timeout(self, ctx, member: discord.Member, duration: str = '1h', *, reason='bad boi'):  # TODO: Fix this
-    #     if member == ctx.message.author:
-    #         raise error_messages.YouMadeAMistake(value=member.display_name,
-    #                                              message="Why do you think you have been a naughty boy 👄")
-    #     if convert(duration) > 2419200 or convert(duration) < 0:
-    #         raise error_messages.YouMadeAMistake(value=duration, message="Timeout can be between 1 second and 28 days")
-    #     await member.timeout(until=datetime.timedelta(seconds=convert(duration)), reason=reason)
-    #     success_message = await ctx.send(embed=success())
-    #     await success_message.add_reaction('✔')
+    # @commands.check_any(commands.has_permissions(
+    # kick_members=True), commands.is_owner()) async def timeout(self, ctx, member: discord.Member, duration: str =
+    # '1h', *, reason='bad boi'):  // Fix this if member == ctx.message.author: raise
+    #  error_messages.YouMadeAMistake(value=member.display_name, message="Why do you think you have been a naughty
+    #  boy 👄") if convert(duration) > 2419200 or convert(duration) < 0: raise error_messages.YouMadeAMistake(
+    #  value=duration, message="Timeout can be between 1 second and 28 days") await member.timeout(
+    #  until=datetime.timedelta(seconds=convert(duration)), reason=reason) success_message = await ctx.send(
+    #  embed=success()) await success_message.add_reaction('✔')
 
     @commands.command(help="Purge the Last `x` messages")
     @commands.check_any(commands.has_permissions(manage_messages=True), commands.is_owner())
     async def purge(self, ctx, number_messages: int = 10):
         await ctx.message.delete()
         await ctx.message.channel.purge(limit=number_messages)
+        success_message = await ctx.send(embed=success())
+        await success_message.add_reaction('✔')
+
+    @commands.command(help="Purge all messages from a user")
+    @commands.check_any(commands.has_permissions(manage_messages=True), commands.is_owner())
+    async def purge_user(self, ctx, member: discord.Member, number_messages: int = 150):
+        passed_before = ctx.message
+
+        await ctx.channel.purge(limit=number_messages, before=passed_before, check=lambda m: (m.author == member and m.created_at >= passed_before.created_at - datetime.timedelta(days=2)))
+
         success_message = await ctx.send(embed=success())
         await success_message.add_reaction('✔')
 
@@ -109,6 +118,23 @@ class Moderation(commands.Cog):
         else:
             raise error_messages.YouMadeAMistake(value=role.name, message=f"Get on {member.display_name}'s level")
 
+    @commands.command(help="Create a Poll")
+    async def poll(self, ctx, question: str, *options):
+        if len(options) < 2:
+            raise error_messages.YouMadeAMistake(value=options, message="You need at least 2 options")
+        if len(options) >= 10:
+            raise error_messages.YouMadeAMistake(value=options, message="You can't have more than 10 options")
+        if len(question) > 200:
+            raise error_messages.YouMadeAMistake(value=question, message="Your question is too long")
+        if len(options) > 1:
+            embed = discord.Embed(title=question, color=0x00ff00)
+            for i in range(len(options)):
+                embed.add_field(name=f"{NUM_DICT[i]} - {options[i]}", value="‎", inline=True)
+            msg = await ctx.send(embed=embed)
+            for i in range(len(options)):
+                await msg.add_reaction(NUM_DICT[i])
+        else:
+            raise error_messages.YouMadeAMistake(value=options, message="You need at least 2 options")
 
 def setup(client):
     client.add_cog(Moderation(client))
