31e3ea4176bf1fcadd6f3a5f20636acae6b32baa
Aryan-401 <aryan.raj.garg@gmail.com>
2022-07-05 15:05:22 +0530

    Updated to Oauth 2.0

--- twitter/tweet.py
+++ twitter/tweet.py
@@ -1,31 +1,34 @@
 import datetime
-import tweepy
+import tweepy  # TODO: Might need to change tokens
 import sqlite3
 from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv('../.env')
 
-oauth = tweepy.OAuthHandler(getenv('CONSUMER_KEY'), getenv('CONSUMER_SECRET'))
-oauth.set_access_token(getenv("ACCESS_TOKEN"), getenv("ACCESS_SECRET"))
-
-api = tweepy.API(oauth)
+client = tweepy.Client(consumer_key=getenv('CONSUMER_KEY'),
+                       consumer_secret=getenv('CONSUMER_SECRET'),
+                       access_token=getenv("ACCESS_TOKEN"),
+                       access_token_secret=getenv("ACCESS_SECRET"))
 
 connection = sqlite3.connect('../twitterBot.db')
 cursor = connection.cursor()
-news_list = list(cursor.execute('''SELECT * FROM News ORDER BY CASE WHEN SUBREDDIT = 'usersub' THEN 1 ELSE 2 END, IMPORTANCE DESC LIMIT 10'''))
+news_list = list(cursor.execute(
+    '''SELECT * FROM News ORDER BY CASE WHEN SUBREDDIT = 'usersub' THEN 1 ELSE 2 END, IMPORTANCE DESC LIMIT 10'''))
 cursor.close()
 for index in range(len(news_list)):
     if news_list[index][1] == 'usersub':
         news_list.insert(-1, news_list.pop(index))
 
 last_tweet = None
-tweet = ''
 for news_item in news_list:
     if news_item[1] == 'usersub' and last_tweet is not None:
-        tweet += "[USER SUBMITTED] "
-    if last_tweet is None:
+        tweet = "[USER SUBMITTED] "
+    elif last_tweet is None:
         utc = datetime.datetime.utcnow()
         tweet = f'It\'s Tech News Time for the week [{(utc - datetime.timedelta(days=7)).strftime("%m/%d/%Y")}] - [{utc.strftime("%m/%d/%Y")}]:\n'
-    last_tweet = api.update_status(status=f'{tweet}{news_item[3]} ({news_item[6]})\nVia:{news_item[4]}',
-                                   in_reply_to_status_id=last_tweet.id, auto_populate_reply_metadata=True)
+    else:
+        tweet = ''
+    last_tweet = client.create_tweet(text=f'{tweet}{news_item[3]} ({news_item[8]})\nVia:{news_item[4]}',
+                                     in_reply_to_tweet_id=int(
+                                         last_tweet.data['id']) if last_tweet is not None else None)
